ea6b31cd5f77670af15a2a4fb2850b847075668f
ExFoxArbitrage <115371023+ExFoxArbitrage@users.noreply.github.com>
2022-10-23 19:34:48 +0300

    Update 3proxyinstall.sh

--- 3proxyinstall.sh
+++ 3proxyinstall.sh
@@ -1,7 +1,9 @@
+version=0.9.4
+apt-get update && apt-get -y upgrade
 apt-get install gcc make git -y
-wget --no-check-certificate https://github.com/ExFoxArbitrage/3proxy/raw/main/3proxy.tar.gz
-tar xvf 3proxy.tar.gz
-cd 3proxy
+wget --no-check-certificate -O 3proxy-${version}.tar.gz https://github.com/z3APA3A/3proxy/archive/${version}.tar.gz
+tar xzf 3proxy-${version}.tar.gz
+cd 3proxy-${version}
 make -f Makefile.Linux
 cd src
 mkdir /etc/3proxy/
