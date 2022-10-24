50bd2c949a7af8af3afd9d7d1bf8aee8e413446e
ExFoxArbitrage <115371023+ExFoxArbitrage@users.noreply.github.com>
2022-10-24 03:53:47 +0300

    Update 3proxyinstall.sh

--- 3proxyinstall.sh
+++ 3proxyinstall.sh
@@ -11,9 +11,6 @@ mv 3proxy /etc/3proxy/
 cd /etc/3proxy/
 wget --no-check-certificate https://raw.githubusercontent.com/ExFoxArbitrage/3proxy/main/3proxy.cfg
 chmod 600 /etc/3proxy/3proxy.cfg
-mkdir /var/log/3proxy/
-wget --no-check-certificate https://raw.githubusercontent.com/ExFoxArbitrage/3proxy/main/.proxyauth
-chmod 600 /etc/3proxy/.proxyauth
 cd /etc/init.d/
 wget --no-check-certificate  https://raw.githubusercontent.com/ExFoxArbitrage/3proxy/main/3proxy
 chmod  +x /etc/init.d/3proxy
